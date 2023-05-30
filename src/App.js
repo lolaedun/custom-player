681c4e97d078f52d83a6da15700645d6d57e0b87
Lola <78376398+lolaedun@users.noreply.github.com>
2023-05-30 17:54:13 +0100

    add boiler plate

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Lola Testing
         </a>
       </header>
     </div>
